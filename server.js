8d88e6af08b8beeaea2a15d96d5dec1d12709bc6
VLCG <harikpalem@gmail.com>
2026-05-09 23:14:31 +0530

    Fix Render port

--- server.js
+++ server.js
@@ -452,8 +452,7 @@ app.get("/export/leaves", requireLogin, (req, res) =>
   )
 );
 
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
