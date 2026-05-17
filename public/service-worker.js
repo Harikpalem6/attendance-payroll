75c6d3945a97b5198287b9df60dcf1152a50af38
VLCG <harikpalem@gmail.com>
2026-05-17 22:36:17 +0530

    Add PWA service worker

---
+++ public/service-worker.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = "vlcg-hrms-pwa-v1";
+
+const STATIC_ASSETS = [
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(STATIC_ASSETS);
+    })
+  );
+
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((cacheName) => cacheName !== CACHE_NAME)
+          .map((cacheName) => caches.delete(cacheName))
+      );
+    })
+  );
+
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin !== location.origin) {
+    return;
+  }
+
+  if (
+    requestUrl.pathname === "/manifest.json" ||
+    requestUrl.pathname.startsWith("/icons/")
+  ) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request);
+      })
+    );
+  }
+});
